1d1b806dbf5926db571488b392849a52ec49abf5
lenaagbegninou <agblala02@gmail.com>
2026-03-13 11:56:38 +0000

    Décentralisation des services :Ajout de l'espace des responsables de saisie de notes

--- frontend_next/src/services/api.js
+++ frontend_next/src/services/api.js
@@ -2,9 +2,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  //baseURL: "http://localhost:8000/api",
   //baseURL: 'http://172.20.1.22/api', 
-  //baseURL: 'https://epl.univ-lome.tg/api',
+  baseURL: 'https://epl.univ-lome.tg/api',
  // baseURL: "/api/",
 
   timeout: 0, // ✅ Pas de timeout  
